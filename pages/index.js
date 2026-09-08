039f0c554eff7d5c616751ff103f884b18e18c9c
SVGVENTURES <guptasoham794@gmail.com>
2026-09-08 14:04:17 +0530

    new changes//

--- pages/index.js
+++ pages/index.js
@@ -99,7 +99,7 @@ const RoomBookingSystem = () => {
     currentDate.setDate(currentDate.getDate() + days);
     const newDateString = currentDate.toISOString().split('T')[0];
     
-    if (newDateString >= getTodayDateString()) {
+    if (new Date(newDateString) >= new Date(getTodayDateString())) {
       setSelectedDate(newDateString);
       setSelectedSlots({}); // Reset selections on date change
       setError("");
